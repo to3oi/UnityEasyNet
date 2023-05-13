13fa28d5a45bab3c7b53c1204c64d6bed6305272
Ito Tomoya <pengyeyiteng@gmail.com>
2023-05-13 15:38:41 +0900

    Add:UDPReceiver_Sample

---
+++ Assets/Plugin/UnityEasyNet/Samples/UDPReceiver/Scripts/UDPReceiver_Sample.cs
@@ -0,0 +1,128 @@
+using System;
+using System.Net;
+using UnityEasyNet;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UDPReceiver_Sample : MonoBehaviour
+{
+
+    private UDPReceiver mUDPReceiver;
+    
+    [SerializeField] private Text mLocalIPAddress;
+
+    [SerializeField] private InputField mReceivePort;
+    [SerializeField] private Image mOnlyPortConnectImage;
+    [SerializeField] private Button mOnlyPortConnectButton;
+
+    [SerializeField] private Image mAnyPortConnectImage;
+    [SerializeField] private Button mAnyPortConnectButton;
+
+    [SerializeField] private Text mReceiveText;
+
+    private int mReciveCount = 0;
+    private bool mConected = false;
+
+    void Start()
+    {
+        //IPv4のアドレスを取得して表示
+        IPHostEntry ipHostEntry = Dns.GetHostEntry(Dns.GetHostName());
+
+        foreach (IPAddress ip in ipHostEntry.AddressList)
+        {
+            if (ip.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
+            {
+                mLocalIPAddress.text = ip.ToString();
+                break;
+            }
+        }
+
+        mReceiveText.text = "";
+        
+        mOnlyPortConnectButton.onClick.AddListener(OnlyConnect);
+        mAnyPortConnectButton.onClick.AddListener(AnyConnect);
+
+    }
+
+    /// <summary>
+    /// 指定したポートで接続開始
+    /// </summary>
+    public void OnlyConnect()
+    {
+        if (mConected)
+        {
+            return;
+        }
+
+        mConected = true;
+
+        try
+        {
+            //UDPReceiverの接続処理
+            mUDPReceiver = new UDPReceiver(int.Parse(mReceivePort.text),ReceiveDataUpdate);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+
+        mOnlyPortConnectImage.color = Color.green;
+    }
+
+    /// <summary>
+    /// すべてのポートで接続開始
+    /// </summary>
+    public void AnyConnect()
+    {
+        //実装中
+        return;
+        
+        if (mConected)
+        {
+            return;
+        }
+
+        mConected = true;
+
+        try
+        {
+            //UDPReceiverの接続処理
+            mUDPReceiver = new UDPReceiver(int.Parse(mReceivePort.text),ReceiveDataUpdate);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+
+        mAnyPortConnectImage.color = Color.green;
+    }
+    
+    
+    void ReceiveDataUpdate(String s)
+    {
+        if (mConected)
+        {
+            return;
+        }
+
+        mReciveCount++;
+
+        if (10 <= mReciveCount)
+        {
+            string str = mReceiveText.text;
+            string delimiter = "\n";
+            int index = str.IndexOf(delimiter);
+            if (index != -1)
+            {
+                mReceiveText.text = str.Substring(index + delimiter.Length);
+            }
+        }
+        else
+        {
+            mReceiveText.text += s;
+            mReceiveText.text += "\n";
+        }
+    }
+}
