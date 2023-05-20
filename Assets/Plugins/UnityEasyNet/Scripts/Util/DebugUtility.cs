08f4a233e49d2c9a144eb43b7a709c47d05e1764
Ito Tomoya <pengyeyiteng@gmail.com>
2023-05-20 17:29:51 +0900

    Move:Util

---
+++ Assets/Plugins/UnityEasyNet/Scripts/Util/DebugUtility.cs
@@ -0,0 +1,19 @@
+using System.Diagnostics;
+
+namespace UnityEasyNet
+{
+    public static class DebugUtility
+    {
+        [Conditional("UNITY_EDITOR")]
+        public static void Log(string message)
+        {
+            UnityEngine.Debug.Log(message);
+        }
+
+        [Conditional("UNITY_EDITOR")]
+        public static void LogError(string message)
+        {
+            UnityEngine.Debug.LogError(message);
+        }
+    }
+}
